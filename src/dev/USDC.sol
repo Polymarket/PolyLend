b9ec87469a6a7f6b39bd66228324cb457ca7c9c1
mike <mshrieve@gmail.com>
2024-06-07 12:36:46 -0700

    move USDC into separate file (#8)

---
+++ src/dev/USDC.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {ERC20} from "../../lib/solady/src/tokens/ERC20.sol";
+
+contract USDC is ERC20 {
+    function name() public pure override returns (string memory) {
+        return "USDC";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "USDC";
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
