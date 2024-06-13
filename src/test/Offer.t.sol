2fa8b008aa5187200969a84cd7fc8d79f294e754
mike <mshrieve@gmail.com>
2024-06-12 21:26:35 -0700

    Transfer Reverts (#13)
    * fix tests
    
    * add pr tests
    
    * add minimum duration to request
    
    * make testPow pure
    
    * transfer reverts

--- src/test/Offer.t.sol
+++ src/test/Offer.t.sol
@@ -6,9 +6,7 @@ import {PolyLendTestHelper, Offer} from "./PolyLendTestHelper.sol";
 contract PolyLendOfferTest is PolyLendTestHelper {
     uint256 rate;
 
-    function test_PolyLend_offer(uint128 _amount, uint128 _loanAmount, uint256 _rate, uint256 _minimumDuration)
-        public
-    {
+    function test_PolyLend_offer(uint128 _amount, uint128 _loanAmount, uint256 _rate, uint32 _minimumDuration) public {
         vm.assume(_amount > 0);
         vm.assume(_minimumDuration <= 60 days);
 
@@ -20,17 +18,15 @@ contract PolyLendOfferTest is PolyLendTestHelper {
         vm.startPrank(borrower);
         conditionalTokens.setApprovalForAll(address(polyLend), true);
 
-        uint256 requestId = polyLend.request(positionId0, _amount);
+        uint256 requestId = polyLend.request(positionId0, _amount, _minimumDuration);
         vm.stopPrank();
 
         vm.startPrank(lender);
         usdc.approve(address(polyLend), _loanAmount);
 
         vm.expectEmit();
-        emit LoanOffered(requestId, lender, _loanAmount, rate, _minimumDuration);
-
-        polyLend.offer(requestId, _loanAmount, rate, _minimumDuration);
-
+        emit LoanOffered(requestId, lender, _loanAmount, rate);
+        polyLend.offer(requestId, _loanAmount, rate);
         vm.stopPrank();
 
         Offer memory offer = _getOffer(0);
@@ -39,6 +35,5 @@ contract PolyLendOfferTest is PolyLendTestHelper {
         assertEq(offer.lender, lender);
         assertEq(offer.loanAmount, _loanAmount);
         assertEq(offer.rate, rate);
-        assertEq(offer.minimumDuration, _minimumDuration);
     }
 }
