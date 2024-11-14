ed36c4b554564dbf20adedb12e57882c2b769272
betmoardotfun <betmoar@proton.me>
2024-11-15 00:15:46 +0800

    Fix: unintended delegatecall external interestLib
    on deployment testing observed that when this function is called it reverts as it attempts to delegatecall to a non existent contract to execute this function. fixed by changing it from public to internal.

--- src/InterestLib.sol
+++ src/InterestLib.sol
@@ -7,7 +7,7 @@ library InterestLib {
     uint256 public constant ONE = 10 ** 18;
     uint256 public constant ONE_THOUSAND_APY = 76_036_763_191;
 
-    function pow(uint256 _base, uint256 _exponent) public pure returns (uint256) {
+    function pow(uint256 _base, uint256 _exponent) internal pure returns (uint256) {
         if (_exponent == 0) {
             return ONE;
         } else if (_exponent % 2 == 0) {
